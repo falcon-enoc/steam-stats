351df9375428da2062ba7f41ab689881011a95a5
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2025-06-03 11:52:39 -0400

    feat. Adds function to retrieve owned games
    Adds a new function to retrieve a user's owned games from the Steam API.
    
    This function fetches the list of games owned by a user, including app info and free-to-play games, using the Steam ID. It then returns the array of games.

--- src/app/services/steamService.ts
+++ src/app/services/steamService.ts
@@ -1,6 +1,6 @@
 // servises/SteamServise.ts
 import { STEAM_KEY } from '../config';        // cargado desde process.env en build
-import type { PlayerSummariesResponse } from '../types/steam';
+import type { PlayerSummariesResponse, OwnedGamesResponse} from '../types/steam';
 import fetcher from '../lib/fetcher';
 const BASE = 'https://api.steampowered.com';
 
@@ -14,4 +14,10 @@ export async function ResolveVanityURL(vanityUrl: string) {
   const url = `${BASE}/ISteamUser/ResolveVanityURL/v1/?key=${STEAM_KEY}&vanityurl=${vanityUrl}`;
   const data = await fetcher<{ response: { steamid: string } }>(url);
   return data.response.steamid;
+}
+
+export async function getOwnedGames(steamId: string): Promise<OwnedGamesResponse['response']['games']> {
+  const url = `${BASE}/IPlayerService/GetOwnedGames/v1/?key=${STEAM_KEY}&steamid=${steamId}&include_appinfo=true&include_played_free_games=true`
+  const data = await fetcher<OwnedGamesResponse>(url)
+  return data.response.games
 }
