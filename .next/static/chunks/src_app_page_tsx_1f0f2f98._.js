756a6de4bc8ed0f7c9a1a6f9e297f98de1fafed5
Enoc Falcón <83475399+falcon-enoc@users.noreply.github.com>
2025-06-16 23:34:12 -0400

    Stop tracking .next directory

--- .next/static/chunks/src_app_page_tsx_1f0f2f98._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_b2e8bd7f._.js",
-  "static/chunks/src_app_85adc777._.js"
-],
-    source: "dynamic"
-});
